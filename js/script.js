84b69cf4469c215a96cc30747405610887ca48a3
Alessio Pancia <alessiop2801@gmail.com>
2020-12-03 15:01:38 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@
 // Utilizzando la funzione forEach e il template literal, visualizzare in pagina
 // tutte le icone con il proprio nome.
 
+// milestone 2:
+// definire un array di colori e associare ad ogni tipo di icona un colore.
+// Visualizzare le icone di colore diverso in base al tipo.
+
 $(document).ready(
   function() {
 
@@ -106,25 +110,37 @@ $(document).ready(
       },
     ];
 
+    const colors = ["green", "red", "lightblue"]
+
+    colorsForType(icons, colors);
     printIcons(icons);
 
+    console.log(icons);
   }
 );
 
 function printIcons(array) {
 
   let htmlElement = $(".icons");
-  // {
-  //   name: "dog",
-  //   prefix: "fa-",
-  //   type: "animal",
-  //   family: "fas"
-  // },
 
   array.forEach(
     function(element, index){
-      let {name, prefix, family} = array[index];
-      htmlElement.append(`<div class="${family} ${prefix}${name}">${name}</div>`)
+      let {name, prefix, family, color} = array[index];
+      htmlElement.append(`<div class="${family} ${prefix}${name}" style="color: ${color};"><span>${name}</span></div>`)
+    }
+  );
+}
+
+function colorsForType(array, colorArray) {
+  array.forEach(
+    function(element, index) {
+      if (element.type == "animal") {
+        element.color = colorArray[0];
+      } else if (element.type == "vehicle") {
+        element.color = colorArray[1];
+      } else if (element.type == "vegetable") {
+        element.color = colorArray[2];
+      }
     }
   );
 }
